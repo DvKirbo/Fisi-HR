0f532a86017cb067c59d0da0510eb3b900a04283
Sebastian ℝ <105824039+SebastianRojas6@users.noreply.github.com>
2023-10-20 17:07:49 -0500

    Commit3

--- html/horarios.js
+++ html/horarios.js
@@ -6,17 +6,14 @@ var horarios = {
     "2-4": [],
     "4-6": []
 };
-
 // Función para agregar una reserva a un horario
 function agregarReserva(horario, reserva) {
     horarios[horario].push(reserva);
 
     // Crear un nuevo Blob con los datos de la reserva
     var blob = new Blob([JSON.stringify(reserva, null, 2)], {type : 'application/json'});
-
     // Crear un nombre para el archivo .txt
     var nombreArchivo = "Reserva_" + horario.replace("-", "a") + "_" + reserva.nombre + ".txt";
-
     // Guardar la reserva en un archivo .txt
     saveAs(blob, nombreArchivo);
 }
